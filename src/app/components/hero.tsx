58967810d6aaedbfb5c378aa15aaab76aa8fff5d
Waheedsadique <115403188+Waheedsadique@users.noreply.github.com>
2023-02-28 19:35:04 +0500

    new

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -11,13 +11,13 @@ const HeroSection = () => {
         <h1 className="text-3xl px-5 pl-15 font-[poppins] text-[#B8162D] ">
           with Panaverse
         </h1>
-        <p className="mx-5 text-center md:text-left font-[poppins]">
+        <p className="px-5 text-center md:text-left font-[poppins]">
           A One and Quarter Years Panaverse DAO Earn as you Learn Program.
           Consolidating Web 3.0, Metaverse, Artificial Intelligence (AI), Cloud,
           Edge, and Ambient Computing/IoT Technologies.
         </p>
-        <div className="py-6">
-        <button className=" px-2 py-2 p-2 text-white  font-[poppins] text-xl bg-[#B8162D] hover:animate-pulse flex rounded-2xl">
+        <div className="py-6 px-5">
+        <button className=" px-5 py-2 p-2 text-white  font-[poppins] text-xl bg-[#B8162D] hover:animate-pulse flex rounded-2xl">
           Join Us
         </button>
         </div>
